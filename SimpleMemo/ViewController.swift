d62bec0fec7624d088e90ecfc597dfcc9b76d09b
Kosuke.S <kosuke_leo@yahoo.co.jp>
2020-07-18 21:01:23 +0900

    Initial Commit

---
+++ SimpleMemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpleMemo
+//
+//  Created by 柴田晃輔 on 2020/07/18.
+//  Copyright © 2020 shibata. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
